33059d2e7194b6ab629dcf87c2f9ee3bd95ad424
Jean Herbaut <jean.herbaut@heig-vd.ch>
2020-02-28 15:26:25 +0100

    plop

--- 20200228/src/index.js
+++ 20200228/src/index.js
@@ -2,5 +2,5 @@ import * as d3 from 'd3'
 import * as DATA from 'C:/Users/Jean/Desktop/DataVis/20200228/json/prepareData.js'
 const body = d3.select('body')
 const ul = body.append('ul');
-
+console.log("Plop");
 let data = new DATA();
