084722a2658b369b0310c4661adaaba37cb368dc
Jean Herbaut <jean.herbaut@heig-vd.ch>
2020-03-06 16:36:06 +0100

    exercice 3

---
+++ 20200306/fetch_rest.js
@@ -0,0 +1,27 @@
+//get datas
+let tabUser = [];
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json =>
+        json.forEach(user => {
+            //console.log(user);
+            tabUser.push(
+                {
+                    "nom_utilisateur": user.name,
+                    "ville": user.address.city,
+                    "nom_companie": user.company.name,
+                    "titres_posts": []
+                }
+            )
+        })
+    )
+console.log(tabUser);
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json()
+        .then(json => json.forEach(element => {
+            console.log(element.title);
+            //tabUser[element.userId].push(element)
+        }))
+    )
+
--- 20200306/src/index.js
+++ 20200306/src/index.js
@@ -1,1 +1,25 @@
-console.log("bonjour");
+//get datas
+const url1 = "https://jsonplaceholder.typicode.com/users";
+const url2 = "https://jsonplaceholder.typicode.com/posts"
+let tabUser = [];
+Promise.all([
+    fetch(url1).then(value => value.json()),
+    fetch(url2).then(value => value.json())
+    ])
+    .then(([users, posts]) => {
+        users.forEach(element => {
+            tabUser.push(
+                {
+                    "nom_utilisateur": element.name,
+                    "ville": element.address.city,
+                    "nom_companie": element.company.name,
+                    "titres_posts": posts.filter(titre => titre.userId == element.id).map(titre => titre.title)
+                }
+            )
+        });
+
+      console.log(tabUser)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
